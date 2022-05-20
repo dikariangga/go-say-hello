e713e028ed96265007bf0f3d9d086e3668c9e03e
dikariangga <riangga321@gmail.com>
2022-05-20 15:34:49 +0700

    Add comment

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,11 @@
 package go_say_hello
 
+/**
+Jika ada perubahan major, contoh menyebabkan error ketika upgrade
+version. Sebaiknya rename nama module. Tambahkan "\v2" dan seterusnya
+agar tidak menimbulkan error ketika pemaikaian.
+*/
+
 func SayHello(name string) string {
 	return "Hello " + name
 }
